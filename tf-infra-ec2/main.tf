93ca24da622802cc669bace81d22b1608ce4129a
devyogi7579 <yogidevelopment37@gmail.com>
2025-04-15 16:45:42 +0200

    Updated with checkov scan to minimize vulnerablities

--- tf-infra-ec2/main.tf
+++ tf-infra-ec2/main.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.subnet_cidr
   availability_zone       = var.availability_zone
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
   tags = {
     Name = "public-subnet"
   }
@@ -51,7 +51,7 @@ resource "aws_route_table_association" "public_subnet" {
   route_table_id = aws_route_table.public.id
 }
 
-# Security Group
+# Security Group - Batch 1
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
@@ -61,14 +61,16 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.168.178.30/32"]         # Replace with your IP or IP range
+    description = "Allow SSH from trusted IP"
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTPS traffic only"
   }
 
   tags = {
@@ -84,6 +86,24 @@ resource "aws_instance" "web" {
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
   key_name                    = "master"
 
+
+monitoring                  = true                   # ✅ CKV_AWS_126
+ebs_optimized               = true                   # ✅ CKV_AWS_135
+
+metadata_options {                                  # ✅ CKV_AWS_79
+    http_tokens              = "required"
+    http_endpoint            = "enabled"
+}
+
+# iam_instance_profile        = aws_iam_instance_profile.web_profile.name  # ✅ CKV2_AWS_41
+
+root_block_device {                                 # ✅ CKV_AWS_8
+    volume_type           = "gp3"
+    volume_size           = 8
+    delete_on_termination = true
+    encrypted             = true
+
+}
   tags = {
     Name = "TerraformEC2"
   }
